c7397e881e8003c296b2ed4b7c96a8beb2501540
petJoD <petitj2024@gmail.com>
2025-04-01 22:23:55 +0200

    more login forwarder code

--- scripts/wfm_scan.js
+++ scripts/wfm_scan.js
@@ -4,6 +4,24 @@ acolyte.wfm_scan = (function(){
 
 
     return {
+        loginCall: function(dataHandler, params) {
+            return new Promise(function(resolve, reject) {
+                $.ajax({
+                    type: "POST",
+                    url: "https://api.writeforme.org/api/v1/login",
+                    data: JSON.stringify(params),
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    success: function(data, textStatus, request) {
+                        dataHandler(data, resolve);
+                    },
+                    error: function(request, status, error) {
+                        alert("WFM Response Error:\n" + request.responseText);
+                        reject();
+                    }
+                })
+            });
+        },
         getSolutions: function(dataHandler, params) {
             return new Promise(function (resolve, reject) {
                 $.ajax({
